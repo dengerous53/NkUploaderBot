108de37ae09f54afb147962d345871299df24a00
NkBots <116135526+NkBots@users.noreply.github.com>
2022-11-24 13:35:48 +0530

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -50,3 +50,42 @@ class Config:
     Extension = ['mp4', 'webm', 'mkv', 'all']
     FORCE_SUB = os.environ.get("FORCE_SUB", "Nkbots") 
     PORT = os.environ.get("PORT", "8080")
+
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001215708095")
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 4194304000
+    TG_MAX_FILE_SIZE = 4194304000
+    FREE_USER_MAX_FILE_SIZE = 4194304000
+
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprcess
+    PROCESS_MAX_TIMEOUT = 300
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
+   
+  
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860535753"))
+    LOGGER = logging
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5467777513"))
+    # Update channel for Force Subscribe
+    
+    TG_MIN_FILE_SIZE = 2097152000
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "NkUploaderBot")
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "5467777513").split()))
+    PRO_USERS.append(OWNER_ID)
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+    BYPASS = os.environ.get("BYPASS", "")
+    ADL_BOT_RQ = {}
+    
